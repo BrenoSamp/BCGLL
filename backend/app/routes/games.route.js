ef09af361af2766348933852a53210d0c988ad67
Brenim <bsampaio8@hotmail.com>
2022-12-18 16:40:27 -0300

    adiciona rota de listagem de jogos por filtro

--- backend/app/routes/games.route.js
+++ backend/app/routes/games.route.js
@@ -21,4 +21,33 @@ gamesRoute.route('/list-most-rated/:consoleId').get((req, res, next) => {
         });
 });
 
+gamesRoute.route('/list-filtered/:consoleId').get((req, res, next) => {
+    var query = {
+        console_id: req.params.consoleId
+    };
+    bodyParams = req.body;
+
+    if (bodyParams.nome) {
+        query.nome = bodyParams.nome
+    }
+
+    if (bodyParams.desenvolvedor) {
+        query.desenvolvedor = bodyParams.desenvolvedor
+    }
+
+    if (bodyParams.genero) {
+        query.genero = bodyParams.genero
+    }
+
+    consoleGames = Games
+        .find(query)
+        .exec(function (err, games) {
+            if (err) {
+                return next(err)
+            } else {
+                res.json(games)
+            }
+        });
+});
+
 module.exports = gamesRoute;
