9b30ce7cead623df5bdb12e9852d7547f1469872
Brenim <bsampaio8@hotmail.com>
2022-12-18 16:26:08 -0300

    adiciona listagem dos 3 jogos mais bem avaliados

--- backend/app/models/models.js
+++ backend/app/models/models.js
@@ -1,6 +1,9 @@
+const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const objectId = schema.ObjectId;
 
-const UsuarioSchema = mongoose.Schema({
+const UsuarioSchema = schema({
     nome: {
         type: String
     },
@@ -15,17 +18,46 @@ const UsuarioSchema = mongoose.Schema({
     collection: 'usuarios'
 });
 
-const ConsolesSchema = mongoose.Schema({
+const ConsolesSchema = schema({
     nome: {
-        type: String
-    }
+        type: objectId
+    },
 }, {
     timestamps: false,
     collection: 'consoles'
 });
 
-module.exports = mongoose.model({
+const GamesSchema = schema({
+    image: {
+        type: String
+    },
+    nome: {
+        type: String
+    },
+    resumo: {
+        type: String
+    },
+    desenvolvedor: {
+        type: String
+    },
+    genero: {
+        type: String
+    },
+    avaliacao: {
+        type: Number
+    },
+    console_id: {
+        type: String
+    }
+
+}, {
+    timestamps: false,
+    collection: 'games'
+});
+
+module.exports = {
     Usuario: mongoose.model('Usuario', UsuarioSchema),
     Consoles: mongoose.model('Consoles', ConsolesSchema),
-    
-});
+    Games: mongoose.model('Games', GamesSchema),
+    mongoose
+};
