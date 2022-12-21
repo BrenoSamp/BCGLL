61253b6ffcc33acc57b5e7c99426e81b8c218861
Leo_Pereira <72778807+LeonadoHP@users.noreply.github.com>
2022-12-20 23:03:43 -0300

    update banco

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use('/avaliacoes', avaliacoesRoute);
 app.use('/consoles', consolesRoute);
 
 const db = require('./app/models/models');
-mongoose.connect('mongodb://127.0.0.1:27017/bcgll', {
+mongoose.connect('mongodb+srv://bcgll:bcgll@bcgll.f4pkm0l.mongodb.net/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
